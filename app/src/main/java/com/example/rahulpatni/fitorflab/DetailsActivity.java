50b04a7d04e7691957c146de1bad18f90b37665b
patnir <rahul20patni@gmail.com>
2017-01-21 04:47:36 -0500

    new activity

---
+++ app/src/main/java/com/example/rahulpatni/fitorflab/DetailsActivity.java
@@ -0,0 +1,20 @@
+package com.example.rahulpatni.fitorflab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+
+    //TextView txtDetailsTitle = (TextView)findViewById(R.id.txtDetailsTitle);
+    //ImageView imgDetailsExcercise = (ImageView)findViewById(R.id.imgDetailsExercise);
+
+
+}
